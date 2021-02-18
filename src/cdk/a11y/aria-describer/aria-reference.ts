889d39fcc7ba32e07415b2efa95c92f7d7283c68
Princend <waxdji3g4y3c04@gmail.com>
2021-02-18 23:38:19 +0800

    aria-reference translate

--- src/cdk/a11y/aria-describer/aria-reference.ts
+++ src/cdk/a11y/aria-describer/aria-reference.ts
@@ -7,11 +7,14 @@
  */
 
 /** IDs are delimited by an empty space, as per the spec. */
+/**根據規範，ID由空格分隔。 */
 const ID_DELIMITER = ' ';
 
 /**
  * Adds the given ID to the specified ARIA attribute on an element.
  * Used for attributes such as aria-labelledby, aria-owns, etc.
+ * 
+ * 將給定ID添加到元素上的指定ARIA屬性。用於諸如aria-labeledby，aria-owns等屬性。
  */
 export function addAriaReferencedId(el: Element, attr: string, id: string) {
   const ids = getAriaReferenceIds(el, attr);
@@ -24,6 +27,7 @@ export function addAriaReferencedId(el: Element, attr: string, id: string) {
 /**
  * Removes the given ID from the specified ARIA attribute on an element.
  * Used for attributes such as aria-labelledby, aria-owns, etc.
+ * 從元素上的指定ARIA屬性中刪除給定的ID。用於諸如aria-labeledby，aria-owns等屬性。
  */
 export function removeAriaReferencedId(el: Element, attr: string, id: string) {
   const ids = getAriaReferenceIds(el, attr);
@@ -39,8 +43,10 @@ export function removeAriaReferencedId(el: Element, attr: string, id: string) {
 /**
  * Gets the list of IDs referenced by the given ARIA attribute on an element.
  * Used for attributes such as aria-labelledby, aria-owns, etc.
+ * 獲取元素上給定的ARIA屬性引用的ID列表。用於諸如aria-labeledby，aria-owns等屬性。
  */
 export function getAriaReferenceIds(el: Element, attr: string): string[] {
   // Get string array of all individual ids (whitespace delimited) in the attribute value
+  // 獲取屬性值中所有單個ID（以空格分隔）的字符串數組
   return (el.getAttribute(attr) || '').match(/\S+/g) || [];
 }

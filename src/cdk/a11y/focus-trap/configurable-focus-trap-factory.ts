12bee060a133d8d4256a2657e4780da7b593e554
princend <waxdji3g4y3c04@gmail.com>
2021-04-14 11:49:07 +0800

    configurable-focus-trap

--- src/cdk/a11y/focus-trap/configurable-focus-trap-factory.ts
+++ src/cdk/a11y/focus-trap/configurable-focus-trap-factory.ts
@@ -6,36 +6,38 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {DOCUMENT} from '@angular/common';
+import { DOCUMENT } from '@angular/common';
 import {
   Inject,
   Injectable,
   Optional,
   NgZone,
 } from '@angular/core';
-import {InteractivityChecker} from '../interactivity-checker/interactivity-checker';
-import {ConfigurableFocusTrap} from './configurable-focus-trap';
-import {ConfigurableFocusTrapConfig} from './configurable-focus-trap-config';
-import {FOCUS_TRAP_INERT_STRATEGY, FocusTrapInertStrategy} from './focus-trap-inert-strategy';
-import {EventListenerFocusTrapInertStrategy} from './event-listener-inert-strategy';
-import {FocusTrapManager} from './focus-trap-manager';
+import { InteractivityChecker } from '../interactivity-checker/interactivity-checker';
+import { ConfigurableFocusTrap } from './configurable-focus-trap';
+import { ConfigurableFocusTrapConfig } from './configurable-focus-trap-config';
+import { FOCUS_TRAP_INERT_STRATEGY, FocusTrapInertStrategy } from './focus-trap-inert-strategy';
+import { EventListenerFocusTrapInertStrategy } from './event-listener-inert-strategy';
+import { FocusTrapManager } from './focus-trap-manager';
 
 /** Factory that allows easy instantiation of configurable focus traps. */
 // 工廠允許輕鬆實例化可配置的焦點陷阱。
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ConfigurableFocusTrapFactory {
   private _document: Document;
   private _inertStrategy: FocusTrapInertStrategy;
 
   constructor(
-      private _checker: InteractivityChecker,
-      private _ngZone: NgZone,
-      private _focusTrapManager: FocusTrapManager,
-      @Inject(DOCUMENT) _document: any,
-      @Optional() @Inject(FOCUS_TRAP_INERT_STRATEGY) _inertStrategy?: FocusTrapInertStrategy) {
+    private _checker: InteractivityChecker,
+    private _ngZone: NgZone,
+    private _focusTrapManager: FocusTrapManager,
+    @Inject(DOCUMENT) _document: any,
+    @Optional() @Inject(FOCUS_TRAP_INERT_STRATEGY) _inertStrategy?: FocusTrapInertStrategy) {
 
     this._document = _document;
     // TODO split up the strategies into different modules, similar to DateAdapter.
+    // 將策略分成不同的模塊，類似於DateAdapter。
+    // inert 惰性
     this._inertStrategy = _inertStrategy || new EventListenerFocusTrapInertStrategy();
   }
 
@@ -44,12 +46,18 @@ export class ConfigurableFocusTrapFactory {
    * @param element The element around which focus will be trapped.
    * @param config The focus trap configuration.
    * @returns The created focus trap instance.
+   * 在給定元素周圍創建一個焦點捕獲區域。
+   * @param元素將圍繞其捕獲焦點的元素。
+   * @param config焦點陷阱配置。
+   * @returns創建的焦點陷阱實例。
    */
   create(element: HTMLElement, config?: ConfigurableFocusTrapConfig): ConfigurableFocusTrap;
 
   /**
    * @deprecated Pass a config object instead of the `deferCaptureElements` flag.
    * @breaking-change 11.0.0
+   * @deprecated傳遞一個配置對象而不是`deferCaptureElements`標誌。
+   * @ breaking-change 11.0.0
    */
   create(element: HTMLElement, deferCaptureElements: boolean): ConfigurableFocusTrap;
 
@@ -63,7 +71,7 @@ export class ConfigurableFocusTrapFactory {
       configObject = config;
     }
     return new ConfigurableFocusTrap(
-        element, this._checker, this._ngZone, this._document, this._focusTrapManager,
-        this._inertStrategy, configObject);
+      element, this._checker, this._ngZone, this._document, this._focusTrapManager,
+      this._inertStrategy, configObject);
   }
 }
--- src/cdk/a11y/focus-trap/configurable-focus-trap.ts
+++ src/cdk/a11y/focus-trap/configurable-focus-trap.ts
@@ -6,21 +6,25 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {NgZone} from '@angular/core';
-import {InteractivityChecker} from '../interactivity-checker/interactivity-checker';
-import {FocusTrap} from './focus-trap';
-import {FocusTrapManager, ManagedFocusTrap} from './focus-trap-manager';
-import {FocusTrapInertStrategy} from './focus-trap-inert-strategy';
-import {ConfigurableFocusTrapConfig} from './configurable-focus-trap-config';
+import { NgZone } from '@angular/core';
+import { InteractivityChecker } from '../interactivity-checker/interactivity-checker';
+import { FocusTrap } from './focus-trap';
+import { FocusTrapManager, ManagedFocusTrap } from './focus-trap-manager';
+import { FocusTrapInertStrategy } from './focus-trap-inert-strategy';
+import { ConfigurableFocusTrapConfig } from './configurable-focus-trap-config';
 
 /**
  * Class that allows for trapping focus within a DOM element.
  *
  * This class uses a strategy pattern that determines how it traps focus.
  * See FocusTrapInertStrategy.
+ * 允許在DOM元素內捕獲焦點的類。
+ * 此類使用一種策略模式來確定如何捕獲焦點。
+ * 請參閱FocusTrapInertStrategy
  */
 export class ConfigurableFocusTrap extends FocusTrap implements ManagedFocusTrap {
   /** Whether the FocusTrap is enabled. */
+  // 是否啟用了FocusTrap。
   get enabled(): boolean { return this._enabled; }
   set enabled(value: boolean) {
     this._enabled = value;
@@ -44,18 +48,21 @@ export class ConfigurableFocusTrap extends FocusTrap implements ManagedFocusTrap
   }
 
   /** Notifies the FocusTrapManager that this FocusTrap will be destroyed. */
+  // 通知FocusTrapManager此FocusTrap將被銷毀。
   destroy() {
     this._focusTrapManager.deregister(this);
     super.destroy();
   }
 
   /** @docs-private Implemented as part of ManagedFocusTrap. */
+  // @ docs-private作為ManagedFocusTrap的一部分實現
   _enable() {
     this._inertStrategy.preventFocus(this);
     this.toggleAnchors(true);
   }
 
   /** @docs-private Implemented as part of ManagedFocusTrap. */
+  // @ docs-private作為ManagedFocusTrap的一部分實現
   _disable() {
     this._inertStrategy.allowFocus(this);
     this.toggleAnchors(false);

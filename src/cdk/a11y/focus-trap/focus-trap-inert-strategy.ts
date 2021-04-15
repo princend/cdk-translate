c30b0bffb9de190cf91d0af63ee7bb5f2ce6b58b
princend <waxdji3g4y3c04@gmail.com>
2021-04-15 09:09:15 +0800

    trap-inert

--- src/cdk/a11y/focus-trap/focus-trap-inert-strategy.ts
+++ src/cdk/a11y/focus-trap/focus-trap-inert-strategy.ts
@@ -8,19 +8,23 @@
 import {
   InjectionToken,
 } from '@angular/core';
-import {FocusTrap} from './focus-trap';
+import { FocusTrap } from './focus-trap';
 
 /** The injection token used to specify the inert strategy. */
+// 用於指定惰性策略的注入令牌。
 export const FOCUS_TRAP_INERT_STRATEGY =
   new InjectionToken<FocusTrapInertStrategy>('FOCUS_TRAP_INERT_STRATEGY');
 
 /**
  * A strategy that dictates how FocusTrap should prevent elements
  * outside of the FocusTrap from being focused.
+ * 指示FocusTrap應如何防止FocusTrap外部的元素被集中的策略。
  */
 export interface FocusTrapInertStrategy {
   /** Makes all elements outside focusTrap unfocusable. */
+  // 使focusTrap之外的所有元素都無法聚焦。
   preventFocus(focusTrap: FocusTrap): void;
   /** Reverts elements made unfocusable by preventFocus to their previous state. */
+  // 將preventFocus使無法聚焦的元素恢復到其先前狀態。
   allowFocus(focusTrap: FocusTrap): void;
 }
